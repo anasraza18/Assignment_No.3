1454cac38bf480c06507db6679587353c00c9d0f
anasraza18 <manasraza18@gmail.com>
2025-01-01 20:57:30 +0500

    Assignment no 3

--- Q.No12.dart
+++ Q.No12.dart
@@ -1,14 +1,23 @@
-void main(){
-
+void main() {
   var i = 1;
-  var sum = 0;
- // List<int> num = [1,2,3,4,5];
 
-  for (i = 1; i <= 5; i++) {
-    
-    
-    var res = (i).toString() * (i+=sum);   
+  for (i = 1; i <= 1; i++) {
+    var res = (i).toString();
     print(res);
-    
+
+    for (var b = 2; b <= 2; b++) {
+      var res1 = b.toString();
+      print(res + res1);
+
+      for (var c = 3; c <= 3; c++) {
+        var res2 = c.toString();
+        print(res + res1 + res2);
+
+        for (var u = 4; u <= 4; u++) {
+          var res3 = u.toString();
+          print(res + res1 + res2 + res3);
+        }
+      }
+    }
   }
 }
