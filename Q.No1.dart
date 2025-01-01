541d81d1ddc6a6e74c42fb03f2513337abccc1a8
anasraza18 <manasraza18@gmail.com>
2025-01-01 13:20:41 +0500

    Assignment no 3

--- Q.No1.dart
+++ Q.No1.dart
@@ -2,12 +2,13 @@ import 'dart:io';
 
 void main(){
 
-  int i = 0;
+  
   stdout.write("Enter the number : ");
   String? input1 = stdin.readLineSync();
   List<int> input = input1!.split(' ').map(int.parse).toList();
+  int i;
 
-  for (var i = 0; i < input.length; i++) {
+  for ( i = 0; i < input.length; i++) {
     
     if(input[i] % 2 == 0){
       print('Even Number is : ${input[i]}');
