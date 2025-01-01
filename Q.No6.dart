006d30d602477845c132bcdd05e885ac7d4758c7
anasraza18 <manasraza18@gmail.com>
2025-01-01 14:41:54 +0500

    Assignment no 3

---
+++ Q.No6.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  String word = 'Hello World';
+  String Vowel = 'aeiou';
+  int count = 0;
+
+  for (var i in word.split('')) {
+    if(Vowel.contains(i)){
+      print('Vowel : ${i}');
+      count++;    
+    }
+  }
+  print('The total vowels are : ${count}');
+}
