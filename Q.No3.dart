7ffebe3684c7cec3d2636a24565f2120eef305a0
anasraza18 <manasraza18@gmail.com>
2025-01-01 13:18:49 +0500

    Assignment no 3

---
+++ Q.No3.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main(){
+
+  stdout.write("Enter the number : ");
+  String? input1 = stdin.readLineSync();
+  List<int> input = input1!.split(' ').map(int.parse).toList();
+  
+  int i = 0;
+  int sum = 0;
+
+  while (i < input.length) {
+    
+    sum = sum + input[i];
+    i++;
+    print(sum);
+  }
+}
