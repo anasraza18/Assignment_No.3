06dce70c336b089f9e1ed2f0eeb079c8e2b7dc4b
anasraza18 <manasraza18@gmail.com>
2025-01-01 20:23:55 +0500

    Assignment no 3

---
+++ Q.No15.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void main(){
+
+  stdout.write("Enter the number : ");
+  String? input1 = stdin.readLineSync();
+  List<int> input = input1!.split(' ').map(int.parse).toList();
+
+  for (var i in input) {
+    if(i > 5){
+      print(i);
+    }
+    
+  }
+}
