84e715968f37fa3313de6d90e2e6eff5e1828157
anasraza18 <manasraza18@gmail.com>
2025-01-01 14:27:47 +0500

    Assignment no 3

---
+++ Q.No5.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main(){
+
+  stdout.write("Enter the number : ");
+  String? innput = stdin.readLineSync();
+  int input = int.parse(innput!);
+
+  for (var i = 1; i <= 10; i++) {
+    print('$input x $i = ${i*input}');
+  }
+}
