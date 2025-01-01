03dcb0b619f5a58355a6ca7fc75868ae2489d24d
anasraza18 <manasraza18@gmail.com>
2025-01-01 20:02:59 +0500

    Assignment no 3

---
+++ Q.No12.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  var i = 1;
+  var sum = 0;
+ // List<int> num = [1,2,3,4,5];
+
+  for (i = 1; i <= 5; i++) {
+    
+    
+    var res = (i).toString() * (i+=sum);   
+    print(res);
+    
+  }
+}
---
+++ Q.No14.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main(){
+
+  String password = 'Anas123';
+  String email = 'manasraza18@gmail.com';
+
+  do {
+    stdout.write("Enter the password : ");
+    String? input = stdin.readLineSync();
+
+    stdout.write("Enter the Email : ");
+    String? input1 = stdin.readLineSync();
+
+    if(input != password){
+       print('Incorrect Password!');
+    }
+    if(input1 != email){
+       print('Incorrect email!');
+    }
+    if(input == password && input1 == email){
+       print('Successfully Login!');
+       break;
+    }
+
+    
+  } while (true);
+
+}
